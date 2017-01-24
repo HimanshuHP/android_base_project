7a6f8a32d49a27cb9b8b7aa9c8e591a320732c9a
HimanshuHP <him.phogat123@gmail.com>
2017-01-24 12:10:29 +0530

    added different product flavors configuration

---
+++ app/src/production/java/com.demo.himanshu.baseproject/utils/BaseConfiguration.java
@@ -0,0 +1,10 @@
+package com.demo.himanshu.baseproject.utils;
+
+/**
+ * Created by himanshu on 24/01/17.
+ */
+
+public class BuildVariantFactory {
+
+    public static final String BASE_URL = "prod_url";
+}
---
+++ app/src/staging/java/com.demo.himanshu.baseproject/utils/BaseConfiguration.java
@@ -0,0 +1,9 @@
+package com.demo.himanshu.baseproject.utils;
+
+/**
+ * Created by himanshu on 24/01/17.
+ */
+
+public class BaseConfiguration {
+    public static final String BASE_URL = "staging_url";
+}
